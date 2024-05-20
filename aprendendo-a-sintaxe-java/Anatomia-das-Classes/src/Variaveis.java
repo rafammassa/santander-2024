9d8778de1f827f428ed6220d04f84bacc30f73cd
rafammassa <rafaella_massa@hotmail.com>
2024-05-20 15:59:01 -0300

    Apresentando algumas variáveis

---
+++ aprendendo-a-sintaxe-java/Anatomia-das-Classes/src/Variaveis.java
@@ -0,0 +1,8 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        String BR = "Brasil";
+        double PI = 3.14;
+        int estadosBrasileiros = 27;
+        int ano = 2000;
+    }
+}
