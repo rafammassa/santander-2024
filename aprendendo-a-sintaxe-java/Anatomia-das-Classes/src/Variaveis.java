e3dbda40476244d0b14462892779d1a1e2e1aee3
rafammassa <rafaella_massa@hotmail.com>
2024-06-04 13:17:57 -0300

    Introduzindo método

--- aprendendo-a-sintaxe-java/Anatomia-das-Classes/src/Variaveis.java
+++ aprendendo-a-sintaxe-java/Anatomia-das-Classes/src/Variaveis.java
@@ -4,5 +4,19 @@ public class Variaveis {
         double PI = 3.14;
         int estadosBrasileiros = 27;
         int ano = 2000;
+
+        String nome = "Rafaella";
+        String sobrenome = "Massa";
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+        System.out.println(nomeCompleto);
+
+        int anoDeFabricaca0 = 2022;
+
+        boolean verdadeira = true;
+    }
+    
+        //introduzindo método
+    public static String nomeCompleto (String nome, String sobrenome){
+        return nome.concat(" ").concat(sobrenome);
     }
 }
