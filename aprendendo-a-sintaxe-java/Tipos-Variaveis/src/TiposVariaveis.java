a9338beab080307f1d7e29a295b1b89309fca7ea
rafammassa <rafaella_massa@hotmail.com>
2024-06-06 14:32:46 -0300

    Anotações sobre constantes

--- aprendendo-a-sintaxe-java/Tipos-Variaveis/src/TiposVariaveis.java
+++ aprendendo-a-sintaxe-java/Tipos-Variaveis/src/TiposVariaveis.java
@@ -4,12 +4,12 @@ public class TiposVariaveis {
         short ano = 2024;
         int cep = 21070333;
         long cpf = 12345678911L;
-        float pi = 3.1415926f;
+        final float PI = 3.1415926f; //final torna em constante e o nome da variável deve ser MAIÚSCULO
 
         System.out.println("Minha idade: " + idade);
         System.out.println("Ano de Nascimento: " + ano);
         System.out.println("Cep: " + cep);
         System.out.println("Cpf: " + cpf);
-        System.out.println("Valor de PI: " + pi);
+        System.out.println("Valor de PI: " + PI);
     }
 }
