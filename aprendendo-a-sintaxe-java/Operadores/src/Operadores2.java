f3c2c248d167a5051d88540138587204e65df11e
rafammassa <rafaella_massa@hotmail.com>
2024-06-06 16:28:37 -0300

    Exemplos de operadores lógicos

---
+++ aprendendo-a-sintaxe-java/Operadores/src/Operadores2.java
@@ -0,0 +1,43 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int a,b;
+
+        a = 5;
+        b = 6;
+        String resultado;
+
+        if (a == b) {
+            resultado = "verdadeiro";
+        } else {
+            resultado = "falso";
+        }
+        System.out.println(resultado);
+
+        a = 6;
+        String resultado2 = a==b ?"verdadeiro":"falso"; //forma resumida do ex acima
+        System.out.println(resultado2);
+
+        //operadores relacionais
+        int numero1, numero2;
+        numero1 = 1;
+        numero2 = 2;
+
+        if (numero1 > numero2){
+            System.out.println("Número 1 > Número 2");
+        }
+        if (numero1 < numero2){
+            System.out.println("Número 1 < Número 2");
+        }
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("São iguais? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("São diferentes? " + simNao);
+
+        String nome1 = "Rafaella";
+        String nome2 = new String("Rafaella");
+        System.out.println(nome1==nome2);
+        System.out.println(nome1.equals(nome2));
+    }
+}
