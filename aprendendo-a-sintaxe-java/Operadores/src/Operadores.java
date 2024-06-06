cf742435ac353e76c13ea1dae81f82455bd40ac4
rafammassa <rafaella_massa@hotmail.com>
2024-06-06 15:42:39 -0300

    Operando com sinal negativo

--- aprendendo-a-sintaxe-java/Operadores/src/Operadores.java
+++ aprendendo-a-sintaxe-java/Operadores/src/Operadores.java
@@ -16,5 +16,19 @@ public class Operadores {
 
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
+
+        //negação
+
+        int numero = 5;
+        System.out.println(-numero);
+        System.out.println(numero);
+        System.out.println(--numero);
+        System.out.println(numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
     }
 }
