4d536ff72d3decbb54bcfcaa00b126679a8c8ab1
rafammassa <rafaella_massa@hotmail.com>
2024-06-10 17:37:33 -0300

    Terceira aula - Aprimorando a condicional composta para encadeada

--- Condicionais-Java/src/ResultadoEscolar.java
+++ Condicionais-Java/src/ResultadoEscolar.java
@@ -5,6 +5,8 @@ public class ResultadoEscolar {
 
         if (nota >= 7){
             System.out.println("Aprovado");
+        } else if (nota >= 5){
+            System.out.println("Recuperação");
         } else {
             System.out.println("Reprovado");
         }
