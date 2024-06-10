1ee34e8f7e8ba5a0f7323866b525a1bf8a95a679
rafammassa <rafaella_massa@hotmail.com>
2024-06-10 15:13:26 -0300

    Adicionando e testando métodos da SmartTv

--- aprendendo-a-sintaxe-java/Metodos/src/Usuario.java
+++ aprendendo-a-sintaxe-java/Metodos/src/Usuario.java
@@ -6,5 +6,17 @@ public class Usuario {
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal: " + smartTv.canal);
         System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal: " + smartTv.canal);
     }
 }
