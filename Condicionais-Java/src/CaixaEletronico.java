146194177c265b09feec3c1b1892309a8b6e93a7
rafammassa <rafaella_massa@hotmail.com>
2024-06-10 17:31:53 -0300

    Segunda aula - Aprimorando a condicional simples para composta

--- Condicionais-Java/src/CaixaEletronico.java
+++ Condicionais-Java/src/CaixaEletronico.java
@@ -1,10 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 26.0;
 
         if (valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Operação concluída");
+        } else {
+            System.out.println("Saldo insuficiente");
         }
         System.out.println("Saldo atual: " + saldo);
     }
