3a610f3a13f5e9bfb7afcfeab93201329d9fb2ee
rafammassa <rafaella_massa@hotmail.com>
2024-06-11 10:41:42 -0300

    Aula 1 - exemplo FOR

---
+++ repeticao-java/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        System.out.println("Contando carneirinhos: ");
+        for (int carneirinho = 1; carneirinho <= 20; carneirinho++) {
+            System.out.print(carneirinho + "... ");
+        }
+        System.out.println("Joãozinho dormiu!");
+    }
+}
